f3809e83a8012f03c00b67adc550d07c64986aa4
radoslaw.jeruzal <radoslaw.jeruzal@xcaliber.com>
2018-07-31 16:08:48 +0200

    #1 feat(Structure): few basic methods to play with DB, which will be used in #2

--- sample.py
+++ sample.py
@@ -1,46 +1,78 @@
 import pandas as pd
-import matplotlib
-import matplotlib.pyplot as plt
+import datetime
 import numpy as np
-import matplotlib.dates as mdates
-from mpl_finance import candlestick_ochl
-import matplotlib.pyplot as plt
 import plotly.plotly as py
 import plotly.graph_objs as go
 import plotly
 from config import plotly_key, plotly_username
+from pymongo import MongoClient, operations
+
+mongo_client = MongoClient('mongodb://localhost:27017/')
 plotly.tools.set_credentials_file(plotly_username, plotly_key)
 
-matplotlib.use('TkAgg')
 
 rolling_window = 20
 no_of_std = 1.5
 field = 'close'
-time_range = 'D'
+time_range = '5T'
 filename = 'bitstampUSD.csv'
-start_date = '2018-05-25'
-end_date ='2018-12-12'
+start_date = '2018-07-28'
+end_date = '2018-12-12'
+pair_name = 'BTCUSD'
 
 money = 100
 open_trade_type = None
 open_trade_price = None
 open_trade_volume = 0
 
-df = pd.read_csv(filename, names=['ts', 'price', 'volume'])
-df.index = pd.to_datetime(df.ts, unit='s')
-df = df.loc[start_date:end_date]
-df = df['price'].resample(time_range).ohlc()
+def read_csv():
+    df = pd.read_csv(filename, names=['ts', 'price', 'volume'])
+    df.index = pd.to_datetime(df.ts, unit='s')
+    df = df.loc[start_date:end_date]
+    df = df['price'].resample(time_range).ohlc()
+    return df
 
+def save_to_db(symbol, grouping_range, df):
+    collection, collection_name = get_collection(grouping_range, symbol)
+    df['ts'] = pd.to_datetime(df.index)
+    records = df.to_dict('records')
+    ops = [operations.ReplaceOne(
+        filter={"ts": record["ts"]},
+        replacement=record,
+        upsert=True
+    ) for record in records]
+
+    result = collection.bulk_write(ops)
+    print("Saved results", len(records), collection_name)
+
+
+def get_collection(grouping_range, symbol):
+    db = mongo_client['main']
+    collection_name = str.join('_', (symbol, grouping_range))
+    collection = db[collection_name]
+    return collection, collection_name
+
+
+
+def get_data_from_timerange(symbol, grouping_range, ts_start, ts_end):
+    collection, collection_name = get_collection(grouping_range, symbol)
+    iso_start = datetime.datetime.fromtimestamp(ts_start, None)
+    iso_end = datetime.datetime.fromtimestamp(ts_end, None)
+    docs = collection.find({"ts" :  {'$gte': iso_start, '$lt': iso_end}})
+    df = pd.DataFrame(list(docs))
+    df.index = pd.to_datetime(df.ts, unit='s')
+    del df['_id']
+    return df
 
 def plotly_candles(df, name='candles', indicators=None):
     if not indicators:
         indicators = []
     candles = go.Candlestick(x=df.index,
-                           open=df.open,
-                           high=df.high,
-                           low=df.low,
-                           close=df.close,
-                           name=name)
+                             open=df.open,
+                             high=df.high,
+                             low=df.low,
+                             close=df.close,
+                             name=name)
     draw_obj = [candles]
     for indicator in indicators:
         draw_obj.append(
@@ -53,51 +85,48 @@ def plotly_candles(df, name='candles', indicators=None):
         )
     py.plot(draw_obj, filename='simple_candlestick')
 
-def plot_candles():
-    global df
-    # drop the date index from the dateframe
-    df.reset_index(inplace=True)
-    # convert the datetime64 column in the dataframe to 'float days'
-    df.ts = mdates.date2num(df.ts)
-    # make an array of tuples in the specific order needed
-    dataAr = [tuple(x) for x in df[['ts', 'open', 'close', 'high', 'low']].to_records(index=False)]
-    # construct and show the plot
-    fig = plt.figure()
-    ax1 = plt.subplot(1, 1, 1)
-    candlestick_ochl(ax1, dataAr, colorup='k', colordown='r', alpha=0.8)
-    plt.show()
 
-
-#plot_candles()
 from ta import bollinger_hband_indicator
+
+df = read_csv()
+save_to_db(pair_name, time_range, df)
+print(df.tail(5))
+
+del df
+start_date_ts = datetime.datetime.strptime(start_date, "%Y-%m-%d").timestamp()
+end_date_ts = datetime.datetime.strptime(end_date, "%Y-%m-%d").timestamp()
+df = get_data_from_timerange(pair_name, time_range, start_date_ts, end_date_ts)
+print(df.tail(5))
+exit()
+
 rolling_mean = df[field].rolling(rolling_window).mean()
 rolling_std = df[field].rolling(rolling_window).std()
 df['rolling_mean'] = rolling_mean
 df['bollinger_high'] = rolling_mean + (rolling_std * no_of_std)
 df['bollinger_low'] = rolling_mean - (rolling_std * no_of_std)
 
-#plotly_candles(df, 'BTC/USD', ['bollinger_high', 'bollinger_low'])
-
-
 df['position'] = None
+
+
 def close_previous_open_new_trade(type_, price):
     global open_trade_volume, open_trade_type, open_trade_price, money
     if open_trade_type is not None:
         price_diff = price - open_trade_price
-        revenue = -(price_diff * open_trade_volume) if open_trade_type=='sell' else (price_diff * open_trade_volume)
-        money = revenue # get the money back
-        if money<=0:
+        revenue = -(price_diff * open_trade_volume) if open_trade_type == 'sell' else (price_diff * open_trade_volume)
+        money = revenue  # get the money back
+        if money <= 0:
             exit()
         open_trade_type = None
     open_trade_type = type_
     open_trade_price = price
     open_trade_volume = money / price
-    print ("close previous open new")
-    print ("type", open_trade_type)
-    print ("volume", open_trade_volume)
-    print ("price", price)
-    print ("money", money)
-    print ("------------------------")
+    print("close previous open new")
+    print("type", open_trade_type)
+    print("volume", open_trade_volume)
+    print("price", price)
+    print("money", money)
+    print("------------------------")
+
 
 trades_list = []
 previous = None
@@ -128,16 +157,10 @@ df['position'].fillna(method='ffill', inplace=True)
 df['timeframe_return'] = np.log(df[field] / df[field].shift(1))
 df['strategy_return'] = df['timeframe_return'] * df['position']
 
-# Plot the strategy returns
+# count total the strategy returns
 total = df['strategy_return'].cumsum()
-#print("trades_list", trades_list)
-#print("number of trades", len(trades_list))
 print("last total value", total.iloc[-1:])
-#total.plot()
-#plt.show()
-#df[[field, 'bollinger_high', 'bollinger_low', 'position']].plot()
-
-#plt.show()
 
 print(df.tail())
-#quandl - daily data library - easy
+# quandl - daily data library - easy
+plotly_candles(df, pair_name, ['bollinger_high', 'bollinger_low'])
