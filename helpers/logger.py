5287c7c2d9286464a85b6bd0256c8661f8c95fcd
radoslaw.jeruzal <radoslaw.jeruzal@xcaliber.com>
2018-09-03 22:58:21 +0200

    Fixed utcnow. Closes #11.

--- helpers/logger.py
+++ helpers/logger.py
@@ -7,7 +7,7 @@ class CustomJsonFormatter(jsonlogger.JsonFormatter):
         super(CustomJsonFormatter, self).add_fields(log_record, record, message_dict)
         if not log_record.get('ts'):
             # this doesn't use record.created, so it is slightly off
-            now = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+            now = datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
             log_record['ts'] = now
             if log_record.get('level'):
                 log_record['level'] = log_record['level'].upper()
